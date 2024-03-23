5382e231fef08e244f80c938fdc2e327e54f9e4f
VianneyM5 <132180141+VianneyM5@users.noreply.github.com>
2024-03-22 19:38:18 -0600

    First

---
+++ prog03-05.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void main(void)
+{
+   pid_t pid;
+  
+   pid = fork();
+   switch(pid)
+   {
+      case -1:   /* error del fork() */
+         perror("fork");
+         break;
+      case 0:    /* proceso hijo */
+         printf("Proceso %d; padre = %d \n", getpid(), getppid());
+         break;
+      default:   /* padre */
+         printf("Proceso %d; padre = %d \n", getpid(), getppid());
+   }
+}
